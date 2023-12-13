7d746c8c84d6ee268e9e234fd7f133520fba91ee
Anton Tretyakov <tretyakov@ad.viva64.com>
2023-12-13 14:29:48 +0300

    Upload PVS-Studio Beta: JBN:104

--- Formula/pvs-studio-beta.rb
+++ Formula/pvs-studio-beta.rb
@@ -1,7 +1,7 @@
 require File.expand_path("../../Abstract/abstract-pvs-studio", __FILE__)
 
 class PvsStudioBeta < AbstractPvsStudio
-  url "https://cdn.pvs-studio.com/beta/pvs-studio-7.28.77994.6297-macos.tgz pvs-studio-7.28.78067.6307-macos.tgz"
+  url "https://cdn.pvs-studio.com/beta/pvs-studio-7.28.77994.6297-macos.tgz pvs-studio-7.28.78067.6307-macos.tgz pvs-studio-7.28.78083.6309-macos.tgz"
   version "7.28.77994.6297"
-  sha256 "250279879d569f152a96ee8deb774ba81eddfe8b4de35f0409f59766bad2a32a 0b63a9a310f9dc9c9d07b13dcc9d3cfffa8c0c4f5345d72213e5dba1e5e73a13"
+  sha256 "250279879d569f152a96ee8deb774ba81eddfe8b4de35f0409f59766bad2a32a 0b63a9a310f9dc9c9d07b13dcc9d3cfffa8c0c4f5345d72213e5dba1e5e73a13 4067fcbb456df718371447c0b2cd3ac40a270634922d293118d219393d7e6841"
 end
