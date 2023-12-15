31caa0e56c0601e0fc05a4503c9be115e4bc135f
Anton Tretyakov <tretyakov@ad.viva64.com>
2023-12-15 12:29:27 +0300

    Upload PVS-Studio Beta: JBN:183

--- Formula/blame-notifier-beta.rb
+++ Formula/blame-notifier-beta.rb
@@ -1,7 +1,7 @@
 require File.expand_path("../../Abstract/abstract-blame-notifier", __FILE__)
 
 class BlameNotifierBeta < AbstractBlameNotifier
-  url "https://cdn.pvs-studio.com/beta/blame-notifier-7.28.78028.1689-macos.tar.gz blame-notifier-7.28.78067.1690-macos.tar.gz blame-notifier-7.28.78099.1691-macos.tar.gz"
+  url "https://cdn.pvs-studio.com/beta/blame-notifier-7.28.78028.1689-macos.tar.gz blame-notifier-7.28.78067.1690-macos.tar.gz blame-notifier-7.28.78099.1691-macos.tar.gz blame-notifier-7.28.78140.1692-macos.tar.gz"
   version "7.28.78028.1689"
-  sha256 "b546604442c0cce00d068761605ddff1e29c8117e69216fe1dd860cb4defc4b2 e8a6d992154a0d962363b28df48c083f5b10d1ba04e91779155f477d43ab73e1 13b2c5ccf9e3ff37f487d5e72f92aa691199e6bb4ef548684c13c8c772527a1e"
+  sha256 "b546604442c0cce00d068761605ddff1e29c8117e69216fe1dd860cb4defc4b2 e8a6d992154a0d962363b28df48c083f5b10d1ba04e91779155f477d43ab73e1 13b2c5ccf9e3ff37f487d5e72f92aa691199e6bb4ef548684c13c8c772527a1e 689cabbe4ae4f2ef7ca9a52055a6b1382907a99b235462d504fe8cb2b307da8d"
 end
